c0297a09327ddd6d3462d8087602789ff03f2b37
watermandrew <Drew.C.Waterman.18@Dartmouth.edu>
2017-04-29 20:46:38 -0400

    small change

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 
 import Counter from '../containers/counter';
